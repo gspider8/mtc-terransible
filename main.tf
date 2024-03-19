d21f57b383329ac9419ffb90d353545c919892a6
Matthew Lehr <matthewslehr@gmail.com>
2024-03-19 20:00:21 +0000

    use count and count.index for subnets

--- main.tf
+++ main.tf
@@ -1,61 +1,62 @@
-data "aws_availability_zones" "available"{}
+data "aws_availability_zones" "available" {}
 
 resource "random_id" "random" {
-    byte_length = 2
+  byte_length = 2
 }
 
 resource "aws_vpc" "mtc_vpc" {
-    cidr_block = var.vpc_cidr
-    enable_dns_hostnames = true
-    enable_dns_support = true
-    
-    tags = {
-        Name = "mtc_vpc-${random_id.random.dec}"
-        project = "mtc-taj"
-    }
-    lifecycle {
-        create_before_destroy = true
-    }
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name    = "mtc_vpc-${random_id.random.dec}"
+    project = "mtc-taj"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_internet_gateway" "mtc_internet_gateway" {
-    vpc_id = aws_vpc.mtc_vpc.id
-    tags = {
-        Name = "mtc_igw-${random_id.random.dec}"
-        project = "mtc-taj"
-    }
+  vpc_id = aws_vpc.mtc_vpc.id
+  tags = {
+    Name    = "mtc_igw-${random_id.random.dec}"
+    project = "mtc-taj"
+  }
 }
 
 resource "aws_route_table" "mtc_public_rt" {
-    vpc_id = aws_vpc.mtc_vpc.id
-    tags = {
-        Name = "mtc-public"
-        project = "mtc-taj"
-    }
+  vpc_id = aws_vpc.mtc_vpc.id
+  tags = {
+    Name    = "mtc-public"
+    project = "mtc-taj"
+  }
 }
 resource "aws_route" "default_route" {
-    route_table_id = aws_route_table.mtc_public_rt.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+  route_table_id         = aws_route_table.mtc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
 }
 
 resource "aws_default_route_table" "mtc_private_rt" {
   default_route_table_id = aws_vpc.mtc_vpc.default_route_table_id
 
   tags = {
-    Name = "mtc_private"
+    Name    = "mtc_private"
     project = "mtc-taj"
   }
 }
 
 resource "aws_subnet" "mtc_public_subnet" {
-  vpc_id = aws_vpc.mtc_vpc.id
-  cidr_block = var.public_cidrs
+  count                   = 2
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = var.public_cidrs[count.index]
   map_public_ip_on_launch = true
-  availability_zone = data.aws_availability_zones.available.names[0]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 
   tags = {
-    Name = "mtc-public"
+    Name    = "mtc-public-${count.index + 1}"
     project = "mtc-taj"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,10 @@
-variable vpc_cidr {
-    type = string
-    default = "10.124.0.0/16"
+variable "vpc_cidr" {
+  type    = string
+  default = "10.124.0.0/16"
 }
 
 variable "public_cidrs" {
-    type = string
-    default = "10.124.1.0/24"
+  type    = list(string)
+  default = ["10.124.1.0/24", "10.124.1.0/24"]
 }
+
