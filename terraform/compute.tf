4783c3489b4c3ba65b83160c78a3f7ec7c549bd1
Matthew Lehr <matthewslehr@gmail.com>
2024-03-20 16:09:01 +0000

    ec2 ssh key

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -13,6 +13,12 @@ resource "random_id" "mtc_node_id" {
     count = var.main_instance_count
 }
 
+resource "aws_key_pair" "mtc_auth" {
+    key_name = var.key_name
+    public_key = file(var.public_key_path)
+  
+}
+
 resource "aws_instance" "mtc_main" {
     count = var.main_instance_count
     instance_type = var.main_instance_type
@@ -28,3 +34,4 @@ resource "aws_instance" "mtc_main" {
         project = "mtc-taj"
     }
 }
+
